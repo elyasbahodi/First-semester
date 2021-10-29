9792bd6b84d4ae047348df7feb0caebf3de65cde
Elyas Bahodi <10201007@ucn.dk>
2021-10-29 10:50:01 +0200

    added validation towards selecting the an option and consecutively show either weekdays or weekends.:x
    Please enter the commit message for your changes. Lines starting

--- FormData/htmlFormsJS.js
+++ FormData/htmlFormsJS.js
@@ -1,15 +1,41 @@
 document.addEventListener('DOMContentLoaded', e => {
+    
+    const setWeekdayAndWeekendVisibility = (isWeekday, isWeekend) => 
+    {
+        var weekday = document.getElementById('weekdays');
+        var weekend = document.getElementById('weekends');
+
+        if(isWeekday)
+            weekday.style.visibility = 'visible';
+        else
+            weekday.style.visibility = 'collapse';
+
+        if(isWeekend)
+            weekend.style.visibility = 'visible';
+        else
+            weekend.style.visibility = 'collapse';
+    };
+    
     var selectdropdown = document.getElementsByName('WeekdayOrWeekend');
 
-    selectdropdown.addEventListener('onchange', e => {
+    console.log(selectdropdown[0]);
+
+
+    selectdropdown[0].addEventListener('change', e => {
         var selectedValue = e.target.options[e.target.selectedIndex].value;
+        console.log(selectedValue);
         
         switch(selectedValue)
         {
-            case "":
-                
+            case "Weekday":
+                setWeekdayAndWeekendVisibility(true, false);
                 break;
-                
+            case "Weekend":
+                setWeekdayAndWeekendVisibility(false, true);
+                break;
+            default:
+                setWeekdayAndWeekendVisibility(false, false);
+                break;
         }
 
     });
